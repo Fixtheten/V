be64fd0d65ad462911251487321a45412f233202
Fixtheten <127852235+Fixtheten@users.noreply.github.com>
2026-01-28 10:32:59 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const btn = document.getElementById("love-btn");
+const secret = document.getElementById("secret");
+
+btn.addEventListener("click", () => {
+  secret.style.display = "block";
+});
